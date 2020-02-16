7f5e0693fc76a624179b444412c23a3ac5fff2fd
hh <hh@hhmbp15.local>
2020-02-16 18:53:17 +0000

    installed axios via npm install axios, and started writing app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+
+
+
 
 function App() {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get("https://api.nasa.gov/planetary/apod")
+        .then(res => {
+          console.log(res);
+          // setData(res.data);
+        });
+        // .catch(err => console.log(err));
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      {/* <img src={data} alt="placeholder text" /> */}
     </div>
   );
 }
